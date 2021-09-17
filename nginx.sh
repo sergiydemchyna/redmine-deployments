43d06089f29d3948092b74ac5ce1fc2bb897890e
Sergiy <sergiydemchyna@gmail.com>
2021-09-17 22:28:36 +0300

    Add files via upload

---
+++ nginx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get -o Acquire::ForceIPv4=true update -y
+sudo apt-get -o Acquire::ForceIPv4=true upgrade -y
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
+
+sudo apt install nginx -y
+
+sudo tee /etc/nginx/sites-available/default<<EOF
+upstream web_backend {
+        server 10.0.1.32:3000;
+        server 10.0.1.33:3000;
+}
+server {
+listen 80;
+location / {
+proxy_pass http://web_backend;
+}
+}
+EOF
+
+
+sudo service nginx restart
