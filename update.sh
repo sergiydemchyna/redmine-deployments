f4a01fac550616e20573382a8a6a4c10846908ba
Sergiy <sergiydemchyna@gmail.com>
2021-09-18 15:13:48 +0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo kill `cat /opt/redmine/tmp/pids/server.pid`
+sudo tee /opt/redmine/config/database.yml<<EOF
+production:
+  adapter: mysql2
+  database: redminedb
+  host: 35.180.21.38
+  username: redmineuser
+  password: "P@ssW0rD"
+  # Use "utf8" instead of "utfmb4" for MySQL prior to 5.7.7
+  encoding: utf8mb4
+EOF
+cd /opt/redmine/ && sudo bundle exec rails server webrick -e production
