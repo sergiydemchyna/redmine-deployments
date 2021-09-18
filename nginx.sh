e45608af487722306a5a36f2d1a3d7e98a7db1d6
Sergiy <sergiydemchyna@gmail.com>
2021-09-18 10:56:12 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -27,7 +27,7 @@ sudo tee /opt/redmine/config/database.yml<<EOF
 production:
   adapter: mysql2
   database: redminedb
-  host: 10.0.1.129
+  host: 35.180.21.38
   username: redmineuser
   password: "P@ssW0rD"
   # Use "utf8" instead of "utfmb4" for MySQL prior to 5.7.7
