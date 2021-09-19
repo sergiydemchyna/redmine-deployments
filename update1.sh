07cbba55201e76a490da431aace9458fa48804d1
Sergiy <sergiydemchyna@gmail.com>
2021-09-19 11:18:36 +0300

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -1,12 +1,1 @@
 #!/bin/bash
-sudo tee /opt/redmine/config/database.yml<<EOF
-production:
-  adapter: mysql2
-  database: redminedb
-  host: 35.180.21.38
-  username: redmineuser
-  password: "P@ssW0rD"
-  # Use "utf8" instead of "utfmb4" for MySQL prior to 5.7.7
-  encoding: utf8mb4
-EOF
-cd /opt/redmine/ && sudo bundle exec rails server webrick -e production
